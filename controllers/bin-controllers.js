7d0ed9fcf4bff14da0d08e2e1c92379b280ed4f5
judah <landzbej@gmail.com>
2023-05-25 19:23:58 -0400

    postgresdb

--- controllers/bin-controllers.js
+++ controllers/bin-controllers.js
@@ -5,7 +5,7 @@ const { Client } = require("pg");
 
 // postgresql setup
 const client = new Client({
-  connectionString: "postgresql://judah:password@localhost:5432/requestbin"
+  connectionString: "postgresql://judah3:password@localhost:5432/requestbin"
 });
 client.connect();
 
